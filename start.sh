1b3a38b0910fe5e625087d81e6478e6bf0dddf3b
XxJoHnCeExX <55908664+XxJoHnCeExX@users.noreply.github.com>
2020-02-24 17:33:53 -0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+# List of Simulation Commands
+# Author: Juan Raya 815538874
+# SDSU Spring 2020 Senior Design Team #59 (Kaladesh) 
+
+# 1. Move into Project Folder
+cd TFLite
+
+# 2. Run Servo Python Script
+python3 TakePics.py
+
+# 3. Run TensorFlow Lite on Images
+python3 detect.py 
